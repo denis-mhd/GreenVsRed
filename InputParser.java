49e302195eba478ac502d4c88e5cc043d69fd366
Denis <denis_mhd@yahoo.com>
2020-07-28 14:42:12 +0300

    Commit

---
+++ InputParser.java
@@ -0,0 +1,34 @@
+import exception.InputException;
+import model.Cell;
+import model.Color;
+
+// Class used to parse input
+public class InputParser {
+    private final InputException inputException;
+
+    public InputParser(InputException inputException) {
+        this.inputException = inputException;
+    }
+
+    // Method for parsing String[] to integer[]
+    public int[] integerParser(String[] input) {
+        this.inputException.parseValidator(input);
+        int[] parsedInput = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            parsedInput[i] = Integer.parseInt(input[i]);
+        }
+        return parsedInput;
+    }
+
+    // Method creates Object[] based on given task
+    public Cell[] cellRowParser(String[] matrixRowInput) {
+        Cell[] cellRowParser = new Cell[matrixRowInput.length];
+        for (int i = 0; i < matrixRowInput.length; i++) {
+            if (matrixRowInput[i].equals("0")) {
+                cellRowParser[i] = new Cell(Color.RED);
+            } else
+                cellRowParser[i] = new Cell(Color.GREEN);
+        }
+        return cellRowParser;
+    }
+}
---
+++ exception/InputException.java
@@ -0,0 +1,48 @@
+package exception;
+
+// Class for validating inputs
+public class InputException extends Exception {
+
+    public InputException() {
+    }
+
+    // Validate parsing numbers from string
+    public void parseValidator(String[] input) {
+        for (String s : input) {
+            try {
+                Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input! Only numbers");
+            }
+        }
+    }
+
+    // Validates numbers range given in task
+    public void validateNumbersFromFirstLine(int[] input) {
+        int current = input[0];
+        for (int i : input) {
+            if (i < 1 || i > 1000 || current != i) {
+                System.out.println("Invalid input! Supports only equal numbers in range 1-1000");
+                throw new IllegalArgumentException();
+            }
+            current = i;
+        }
+    }
+
+    // Validate numbers which creates the matrix
+    public void validateNumbersForFillingMatrix(int[] input) {
+        for (int i : input) {
+            if (i != 0 && i != 1) {
+                System.out.println("Invalid input! Supports only equal numbers in range 1-1000");
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public void validateNumbersLastInputLine(int x, int y, int rounds, int row, int col) {
+        if (x < 0 || x > row || y < 0 || y > col || rounds < 0) {
+            System.out.println("Wrong input! Must be positive numbers and less than size of grid.");
+            throw new IllegalArgumentException();
+        }
+    }
+}
